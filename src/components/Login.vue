bc154465ca3c71e3e4021a0f9d7d28345c1d062d
Senseinvader <d.ivantsov1983@gmail.com>
2019-04-04 20:18:09 +0200

    Create Login component with the form (dummy)

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login-container">
+    <h2>Yo!</h2>
+    <form class="input-form">
+      <input type="text" v-model="login">
+      <input type="text" v-model="password">
+      <button class="submit-button">SUBMIT</button>
+    </form>
+    <div class="alert">{{alert}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      login: '',
+      password: '',
+      alert: '',
+    };
+  },
+  computed: {
+    // error() {
+    //   return this.$store.error;
+    // },
+  },
+};
+</script>
+
+<style>
+
+</style>
+
