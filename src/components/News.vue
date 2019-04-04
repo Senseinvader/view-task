0b417614d0c99edabf9f3107ef4402f2a4a04005
Senseinvader <d.ivantsov1983@gmail.com>
2019-04-04 17:20:41 +0200

    Add News component and routing to it

---
+++ src/components/News.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p class="text">bla bla bla</p>
+    <br>
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  data() {
+    return {
+      text: 'some text',
+    };
+  },
+};
+</script>
+
+<style>
+  .text {
+    color: red;
+  }
+</style>
