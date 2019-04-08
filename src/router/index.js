ac60959644602881357ed08dd96817ced9eba4d1
Senseinvader <d.ivantsov1983@gmail.com>
2019-04-08 10:46:06 +0200

    Refactor route.index to lazy load components upon routing to them

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,52 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
-import News from '@/components/News';
-import Login from '@/components/Login';
-import Profile from '@/components/Profile';
 import { store } from '@/store/Store';
+
+const routeOptions = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: 'HelloWorld',
+  },
+  {
+    path: '/news',
+    name: 'News',
+    component: 'News',
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: 'Profile',
+    meta: { requiresAuth: true },
+    beforeEnter: (to, from, next) => {
+      if (!store.getters['auth/isAuthenticated']) {
+        next('/login');
+      } else {
+        next();
+      }
+    },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: 'Login',
+  },
+];
+
+const routes = routeOptions.map((route) => {
+  let imports = {};
+  if (route.component) {
+    imports = { component: () => import(`@/components/${route.component}.vue`) };
+  }
+  return {
+    ...route,
+    ...imports,
+  };
+});
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
-    },
-    {
-      path: '/news',
-      name: 'News',
-      component: News,
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: Profile,
-      meta: { requiresAuth: true },
-      beforeEnter: (to, from, next) => {
-        if (!store.getters['auth/isAuthenticated']) {
-          next('/login');
-        } else {
-          next();
-        }
-      },
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login,
-    },
-  ],
+  routes,
 });
